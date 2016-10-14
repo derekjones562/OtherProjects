143297aa225ab80776ae6ea3e4f12fcb602d361c
Derek <djones@doba.com>
2016-10-14 15:28:42 -0600

    update master url file

--- RomanNumeralConverter/RomanNumeralConverter/urls.py
+++ RomanNumeralConverter/RomanNumeralConverter/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-from converter import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^romannumeralconverter/$', views.ConverterView.as_view(), name='converter' ),
-    #url(r'romannumeralconverter/', views.DisplayConversionView.as_view(), name='display'),
+    url(r'^romannumeralconverter/', include('converter.urls')),
+    url(r'^storage/', include('storingNumerals.urls')),
 ]
